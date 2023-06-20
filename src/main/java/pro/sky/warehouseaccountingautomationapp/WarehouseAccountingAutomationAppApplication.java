07e50e78eaf2b54d6154d617aecb10b8d72dce2b
Denis <demaz@bk.ru>
2023-06-20 23:26:19 +0700

    Initial commit

---
+++ src/main/java/pro/sky/warehouseaccountingautomationapp/WarehouseAccountingAutomationAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.warehouseaccountingautomationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAccountingAutomationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseAccountingAutomationAppApplication.class, args);
+    }
+
+}
