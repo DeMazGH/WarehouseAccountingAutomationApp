5b6c820ba9336bcd333f1c78a6fed76908b81c2d
Denis <demaz@bk.ru>
2023-06-21 01:13:16 +0700

    - added repo for Socks

---
+++ src/main/java/pro/sky/warehouseaccountingautomationapp/repository/SocksRepository.java
@@ -0,0 +1,9 @@
+package pro.sky.warehouseaccountingautomationapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.sky.warehouseaccountingautomationapp.model.Socks;
+
+@Repository
+public interface SocksRepository extends JpaRepository<Socks, Long> {
+}
